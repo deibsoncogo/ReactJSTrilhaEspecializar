88eb2a87e7d23fc7e194254525d3bb37a02665d8
Deibson Cogo <deibsoncogotrabalho@outlook.com>
2022-08-10 15:47:59 -0300

    ✨ feat: implementation of class 'Estado'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import { Card } from '../../components/card'
 import style from './style.module.css'
 
 export function Home() {
+  const [studentName, setStudentName] = useState('')
+
   return (
     <div className={style.home}>
-      <h1>Lista de presença</h1>
+      <h1>Nome: {studentName}</h1>
 
-      <input type="text" placeholder='Digite o nome...' />
+      <input
+        type="text"
+        placeholder='Digite o nome...'
+        onChange={event => setStudentName(event.target.value)}
+      />
+
       <button type='button'>Adicionar</button>
 
       <Card name="Rodrigo" time="10:55:25" />
